933da063d40b625fc262491f2c1c494d9d1be727
maya <amacrk@i.softbank.jp>
2020-05-17 14:52:15 +0900

    Test

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+    var number = 0
+    var nameArray: [String] = ["ドサコ", "ジュリー", "ブーケ","ティーナ","ビンタ","バッカス"]
+    var numberArray: [String] = ["1番目","2番目","3番目","4番目","5番目","6番目"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func mae() {
+        number -= 1
+        nameLabel.text = nameArray[number]
+        numberLabel.text = numberArray[number]
+    }
+    
+    @IBAction func tugi() {
+        if number < 6 {
+         number += 1
+        } else if number == 0 {
+            number = 0
+        }
+         nameLabel.text = nameArray[number]
+         numberLabel.text = numberArray[number]
+        
     }
 
 
