246458ea31c246e7bb393e89a8332d8f9d164356
maya <amacrk@i.softbank.jp>
2020-05-17 14:08:33 +0900

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by maya on 2020/05/17.
+//  Copyright © 2020 maya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
